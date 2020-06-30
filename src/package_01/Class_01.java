786894d4986885ef8332349bc05cd06c4c68b722
Busra <girzap92@gmail.com>
2020-06-29 19:20:04 -0500

    feature branch'den ilk commit

--- src/package_01/Class_01.java
+++ src/package_01/Class_01.java
@@ -4,6 +4,9 @@ public class Class_01 {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repo");
+		System.out.println("Ikinci commit from local repo");
+		
+		System.out.println("yeni branch adi feature");
 
 	}
 
